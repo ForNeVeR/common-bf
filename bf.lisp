883eed5816d8589524faf9f950d8bd039052d20b
ForNeVeR <neverthness@gmail.com>
2012-11-18 00:11:45 +0700

    Refactoring, fixed immutability.

--- bf.lisp
+++ bf.lisp
@@ -1,6 +1,14 @@
 (defun new-memory ()
   (make-array 256 :initial-element 0))
 
+(defun get-memory (memory position)
+  (aref memory position))
+
+(defun set-memory (memory position value)
+  (let ((memory (make-array 256 :initial-contents memory)))
+    (setf (aref memory position) value)
+    memory))
+
 (defun bf (source)
   (let ((memory (new-memory))
 	(position 0))
@@ -9,12 +17,21 @@
 				   (setf position (second result))))))
 
 (defun process (command memory position)
-  (let ((memory (copy-array memory)))
-    (cond
-     ((char= command #\<) (decf position))
-     ((char= command #\>) (incf position))
-     ((char= command #\+) (incf (aref memory position)))
-     ((char= command #\-) (decf (aref memory position)))
-     ((char= command #\.) (print (aref memory position)))))
-  (list memory position))
-    
+  ;; Side effects first:
+  (cond 
+   ((char= command #\.) (print (aref memory position))))
+
+  ;; Now - memory and position:
+  (let ((new-position (cond
+		       ((char= command #\<) (- position 1))
+		       ((char= command #\>) (+ position 1))
+		       (t position)))
+	(new-memory (let* ((value (get-memory memory position))
+			   (new-value (cond
+				       ((char= command #\+) (+ value 1))
+				       ((char= command #\-) (- value 1))
+				       (t value))))
+		      (set-memory memory position new-value))))
+
+    ;; Return changed values:
+    (list new-memory new-position)))
